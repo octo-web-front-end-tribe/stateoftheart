afbd88f37e8c6d8a2c3626d0662cbac2d2cd3ca3
William Ong <wong@octo.com>
2015-10-29 14:16:30 +0100

    add project form

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,11 +1,27 @@
 'use strict';
 var db = new (require('../services/database'))();
 
-var projectsController = function() {};
+var projectsController = function () {
+};
 
-projectsController.prototype.displayAll = function(req, res) {
-    db.projects.find().exec().then(function(projects) {
-        res.render('index', {title: 'StateOftheart', projects : projects});
+projectsController.prototype.displayAll = function (req, res) {
+    db.projects.find().exec().then(function (projects) {
+        res.render('index', {title: 'StateOftheart', projects: projects});
+    });
+};
+
+projectsController.prototype.addProjectForm = function (req, res) {
+    res.render('addProject', {title: 'StateOftheart', added: false});
+};
+
+projectsController.prototype.addProject = function (req, res) {
+    var project = req.body;
+    db.projects.create(project, function (err, project) {
+        if (err) {
+            console.error(err);
+        } else {
+            res.render('addProject', {added: true});
+        }
     });
 };
 
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var projectsController = new (require('../controllers/projectsController'))();
 
 router.get('/', projectsController.displayAll);
+router.get('/projects/add', projectsController.addProjectForm);
+router.post('/projects/add', projectsController.addProject);
 
 module.exports = router;
